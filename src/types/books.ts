bd140a02efc30d5e0481e5714f8299a5005701a4
shalakashelly <shalaka.shelly@gmail.com>
2023-09-19 14:36:54 -0400

    - Removing hardcoded values for Catrgories and Types. - camelCase for publication_date and cover_image.

--- src/types/books.ts
+++ src/types/books.ts
@@ -3,44 +3,10 @@ export type TBook = {
   title: string,
   author: string,
   publisher?: string,
-  publication_date?: string,
+  publicationDate?: number,
   isbn?: string,
   description: string,
-  cover_image: string,
-  categories: Category[],
-  tags: Tags[]
+  coverImage: string,
+  categories: string[],
+  tags: string[]
 }
-
-export type Category =
-  "Adventure" |
-  "Classic" |
-  "Fantasy" |
-  "Fiction" |
-  "Humor" |
-  "Magical Realism" |
-  "Modernist" |
-  "Poetry" |
-  "Science Fiction" |
-  "Spiritual";
-
-export type Tags = 
-  "Adventure" |
-  "American literature" |
-  "British literature" |
-  "Classic" |
-  "Coming of age" |
-  "Family saga" |
-  "Gothic" |
-  "Irish literature" |
-  "Latin American literature" |
-  "Magical Realism" |
-  "Philosophical" |
-  "Psychological" |
-  "Quest" |
-  "Romance" |
-  "Russian literature" |
-  "Science Fiction" |
-  "Social commentary" |
-  "Spiritual" |
-  "Stream of consciousness" |
-  "Totalitarianism";
