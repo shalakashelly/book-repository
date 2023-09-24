57a56bc9208b413491cf974496198f7f4b2e29e0
shalakashelly <shalaka.shelly@gmail.com>
2023-09-24 12:52:29 -0400

    - Created a custom hook for fetching books.

--- src/components/Books/books.tsx
+++ src/components/Books/books.tsx
@@ -1,24 +1,9 @@
-import { useState, useEffect } from 'react';
 import BookCard from '../BookCard/BookCard';
-import { TBook } from '../../types/books';
-import { fetchAllBooks } from '../../services';
+import { useBooks } from '../../hooks/useBooks'; // Import the custom hook
+
 
 const Books = () => {
-    const [books, setBooks] = useState<TBook[] | undefined | null>();
-
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                const data = await fetchAllBooks();
-                setBooks(data);
-            } catch (error) {
-                console.log("Error fetching data: ", error);
-                setBooks(null);
-            }
-        }
-
-        fetchData();
-    }, []);
+    const books = useBooks();
 
     if (!books) {
         return <div className="grid-title">Loading...</div>;
