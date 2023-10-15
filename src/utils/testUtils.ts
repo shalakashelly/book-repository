3a4ce63c7ef3aa8dc4a99b79df76ce40cb12995e
Shalaka More <shalaka.shelly@gmail.com>
2023-10-15 15:28:48 -0400

    - Declared global variable in testUtils

--- src/utils/testUtils.ts
+++ src/utils/testUtils.ts
@@ -1,5 +1,9 @@
+declare global {
+    var IS_REACT_ACT_ENVIRONMENT: boolean;
+}
+
 export function ignoreActWarnings() {
-    let prev;
+    let prev: any;
     beforeAll(() => {
         prev = global.IS_REACT_ACT_ENVIRONMENT;
         global.IS_REACT_ACT_ENVIRONMENT = false;
