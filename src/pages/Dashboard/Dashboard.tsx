828486b62ce5e5682e97c94d06ba267b0d9a6c91
Shalaka More <shalaka.shelly@gmail.com>
2023-10-15 15:40:37 -0400

    - Removing comments

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -6,42 +6,37 @@ import styles from '../../App.module.scss';
 
 export default function Dashboard() {
 
-    const getUsername = ( state: { users: UserState }) => {
+    const getUsername = (state: { users: UserState }) => {
         return state.users.userInfo;
     }
     const user = useSelector(getUsername);
 
-    // Function to store the username in local storage
     const storeUsernameInLocalStorage = (username: string) => {
-    localStorage.setItem('username', username);
+        localStorage.setItem('username', username);
     };
 
-    // Function to retrieve the username from local storage
     const getUsernameFromLocalStorage = () => {
-    return localStorage.getItem('username');
+        return localStorage.getItem('username');
     };
 
-    // State to hold the displayed username
     const [displayedUsername, setDisplayedUsername] = useState<string | null>(null);
 
-    // Effect to retrieve and set the displayed username from local storage
     useEffect(() => {
         const storedUsername = getUsernameFromLocalStorage();
         if (storedUsername) {
-        setDisplayedUsername(storedUsername);
+            setDisplayedUsername(storedUsername);
         }
     }, []);
 
-    // Use the storeUsernameInLocalStorage function when user data is available
     useEffect(() => {
         if (user) {
-        storeUsernameInLocalStorage(user.username);
+            storeUsernameInLocalStorage(user.username);
         }
     }, [user]);
 
     function renderContent() {
 
-        if(user || displayedUsername) {
+        if (user || displayedUsername) {
             return (
                 <div className={styles.grid__wrapper}>
                     <p className={styles.grid__title}>Welcome, {user?.username || displayedUsername}!</p>
@@ -52,6 +47,6 @@ export default function Dashboard() {
             return <p className={styles.grid__title}>Please log in</p>
         }
     }
-    
+
     return <div>{renderContent()}</div>;
 }
